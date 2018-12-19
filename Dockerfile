b3f97269ed2a83125b53c09f92abe348214f9533
Ethan Turner <ezturner@washington.edu>
2018-12-18 19:33:47 -0800

    Adding base container and settings

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM ubuntu:18.04
+WORKDIR /app/
+ENV PYTHONUNBUFFERED 1
+RUN apt-get -y update && \
+    apt-get -y install apache2 apache2-dev && \
+    apt-get upgrade -y && \
+    apt-get dist-upgrade -y && \
+    apt-get clean all
+
+# Install system dependencies
+RUN apt-get  update -y&& \
+    apt-get install -y \
+    git \
+    hostname \
+    openssl \
+    sqlite3 \
+    sudo \
+    tar \
+    curl \
+    wget \
+    python-setuptools \
+    build-essential\
+    python3.6-dev \
+    python3-venv \
+    libxml2-dev \
+    libxmlsec1-dev \
+    python-pip \
+    libmysqlclient-dev
+
+RUN python3 -m venv /app/
+RUN . /app/bin/activate && wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py && pip3 install --upgrade pip && pip install mod_wsgi && pip install boto3 watchtower && pip install mysqlclient
+RUN . /app/bin/activate && pip install django && django-admin.py startproject project . && pip uninstall django -y
+ADD /django-scaffold/project /app/project
---
+++ project/base_settings.py
@@ -0,0 +1,187 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+SECRET_KEY = os.getenv("DJANGO_SECRET", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_user_agents',
+]
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.RemoteUserBackend',
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'America/Los_Angeles'
+
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+ROOT_URLCONF = 'project.urls'
+WSGI_APPLICATION = 'project.wsgi.application'
+
+if os.getenv('DB', "sqlite3") == "sqlite3":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+elif os.getenv('DB', "sqlite3") == "mysql":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': os.getenv("DATABASE_HOSTNAME", "localhost"),
+            'NAME': os.getenv("DATABASE_DB_NAME", "db"),
+            'USER': os.getenv("DATABASE_USERNAME", "mysql_user"),
+            'PASSWORD': os.getenv("DATABASE_PASSWORD", "hunter2"),
+        }
+    }
+
+
+if os.getenv("CACHE", "none") == "memcached":
+    RESTCLIENTS_DAO_CACHE_CLASS='myuw.util.cache_implementation.MyUWMemcachedCache'
+    RESTCLIENTS_MEMCACHED_SERVERS = (os.getenv('CACHE_NODE_0', "") + ":" + os.getenv('CACHE_PORT', "11211"), os.getenv('CACHE_NODE_1', "") + ":" + os.getenv('CACHE_PORT', "11211"),)
+
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+COMPRESS_ENABLED = False
+COMPRESS_ROOT = "/static/"
+STATIC_ROOT = "/static/"
+STATIC_URL = '/static/'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'debug':  True,
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        }
+    }
+]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+    },
+    'handlers': {
+        'stdout': {
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'strm': sys.stdout
+        },
+        'stderr': {
+            'level':'ERROR',
+            'class':'logging.StreamHandler',
+            'strm': sys.stderr
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['stdout'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        '': {
+            'handlers': ['stderr'],
+            'level': 'ERROR',
+            'propagate': False,
+        }
+    }
+}
+
+if os.getenv("AUTH", "SAML_MOCK") == "SAML_MOCK":
+    INSTALLED_APPS += ["uw_saml"]
+
+    MOCK_SAML_ATTRIBUTES = {
+    'uwnetid': ['javerage'],
+    'affiliations': ['student', 'member', 'alum', 'staff', 'employee'],
+    'eppn': ['javerage@washington.edu'],
+    'scopedAffiliations': ['student@washington.edu', 'member@washington.edu'],
+    'isMemberOf': ['u_test_group', 'u_test_another_group',
+                   'u_astratest_myuw_test-support-admin'],
+    }
+
+elif os.getenv("AUTH", "SAML_MOCK") == "SAML":
+    INSTALLED_APPS += ["uw_saml"]
+
+    CLUSTER_CNAME = os.getenv("CLUSTER_CNAME", "localhost")
+
+    UW_SAML = {
+        'strict': True,
+        'debug': True,
+        'sp': {
+            'entityId': CLUSTER_CNAME + '/saml',
+            'assertionConsumerService': {
+                'url': CLUSTER_CNAME + '/saml/sso',
+                'binding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST'
+            },
+            'singleLogoutService': {
+                'url': CLUSTER_CNAME + '/saml/logout',
+                'binding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect'
+            },
+            'NameIDFormat': 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
+            'x509cert': os.getenv("SP_CERT", ""),
+                },
+        'idp': {
+            'entityId': 'urn:mace:incommon:washington.edu',
+            'singleSignOnService': {
+                'url': 'https://idp.u.washington.edu/idp/profile/SAML2/Redirect/SSO',
+                'binding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect'
+            },
+            'singleLogoutService': {
+                'url': 'https://idp.u.washington.edu/idp/logout',
+                'binding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect'
+            },
+            'x509cert': os.getenv("IDP_CERT", ""),
+        },
+        'security': {
+            'authnRequestsSigned': False,
+            'wantMessagesSigned': True,
+            'wantAssertionsSigned': False,
+            'wantAssertionsEncrypted': False,
+                }
+    }
+
+    from django.core.urlresolvers import reverse_lazy
+    LOGIN_URL = reverse_lazy('saml_login')
+    LOGOUT_URL = reverse_lazy('saml_logout')
+
+    REMOTE_USER_FORMAT = "uwnetid"
