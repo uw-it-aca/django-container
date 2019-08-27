a37aeef013bb38c6de6dbd657e7ae70361c01658
Ethan Turner <ezturner@uw.edu>
2019-08-27 14:34:11 -0700

    Fixing typo

--- project/base_settings.py
+++ project/base_settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 if os.getenv('ENV', 'localdev'):
     SECRET_KEY = os.getenv('DJANGO_SECRET', get_random_secret_key())
 else:
-    SECREY_KEY = os.getenv('DJANGO_SECRET', None)
+    SECRET_KEY = os.getenv('DJANGO_SECRET', None)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
