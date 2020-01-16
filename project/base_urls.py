591874f0d4900499d2dc144dd3146e9827d17d10
Jim Laney <jlaney@uw.edu>
2020-01-16 11:37:05 -0800

    align urls.py with settings.py for saml env check

--- project/base_urls.py
+++ project/base_urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, re_path
 import os
 
-urlpatterns = [
-]
+urlpatterns = []
 
-if os.getenv("AUTH", "mock") == "SAML" or os.getenv("AUTH", "SAML_MOCK") == "SAML_MOCK":
-    urlpatterns += [ re_path(r'^saml/', include('uw_saml.urls')) ]
+
+if os.getenv('AUTH', '') == 'SAML' or os.getenv('AUTH', '') == 'SAML_MOCK':
+    urlpatterns += [re_path(r'^saml/', include('uw_saml.urls'))]
