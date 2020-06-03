eb655bcd76c1a57b1d2a71358fa71603f82ee62c
mike seibel <mikes@washington.edu>
2020-06-03 13:50:13 -0700

    memcached in readiness probe

--- scripts/ready.sh
+++ scripts/ready.sh
@@ -8,5 +8,13 @@ then
 
 fi
 
-# if exists, deploy.sh successful
-cat /tmp/ready
+if ! [[ -z "$MEMCACHED_SERVER_COUNT" ]]
+then
+
+    MEMCACHED_SERVER=$(echo $MEMCACHED_SERVER_SPEC | sed 's/{}/0/g')
+    echo -e "stats\nquit" | netcat $MEMCACHED_SERVER 11211
+
+fi
+
+# planted in the filesystem by successful deploy.sh
+echo cat /tmp/ready
