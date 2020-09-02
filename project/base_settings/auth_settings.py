9e1ee5ae45a2ef45e191ed3b1cffe26b362c3ee4
mike seibel <mikes@washington.edu>
2020-09-01 20:36:54 -0700

    pass lti sessionid

--- project/base_settings/auth_settings.py
+++ project/base_settings/auth_settings.py
@@ -90,8 +90,14 @@ _auth = auth_from_env('BLTI')
 if _auth:
     INSTALLED_APPS.append('blti')
 
+    MIDDLEWARE.remove('django.middleware.clickjacking.XFrameOptionsMiddleware')
     MIDDLEWARE.insert(0, 'blti.middleware.SessionHeaderMiddleware')
     MIDDLEWARE.insert(0, 'blti.middleware.CSRFHeaderMiddleware')
+    MIDDLEWARE.insert(0, 'blti.middleware.SameSiteMiddleware')
+
+    # relax samesite, but protect cookies from casual snooping
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
 
     LTI_ENFORCE_SSL = parse_bool_from_str(os.getenv('LTI_ENFORCE_SSL', 'False'))
 
