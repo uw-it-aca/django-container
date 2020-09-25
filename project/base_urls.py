f96d6fec34cccad0911594b2445a5257070beb23
mike seibel <mikes@washington.edu>
2020-09-25 13:57:35 -0700

    AUTH may be a list

--- project/base_urls.py
+++ project/base_urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     re_path(r'^', include('django_prometheus.urls'))
 ]
 
-if os.getenv('AUTH', '') in ['SAML', 'SAML_MOCK', 'SAML_DJANGO_LOGIN']:
+if any(item in ['SAML', 'SAML_MOCK', 'SAML_DJANGO_LOGIN'] for item in os.getenv('AUTH', '').split(' ')):
     urlpatterns += [re_path(r'^saml/', include('uw_saml.urls'))]
