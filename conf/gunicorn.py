55c4fd32f551024b7794161fe26fab4f343a3f32
Jim Laney <jlaney@uw.edu>
2020-12-08 18:01:10 -0800

    use stdout/stderr for logs

--- conf/gunicorn.py
+++ conf/gunicorn.py
@@ -144,9 +144,9 @@ tmp_upload_dir = None
 #       A string of "debug", "info", "warning", "error", "critical"
 #
 
-errorlog = '/dev/stderr'
+errorlog = '-'
 loglevel = 'info'
-accesslog = '/dev/stdout'
+accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 #
